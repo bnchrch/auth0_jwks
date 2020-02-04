48406cebc6e86b8dcfcb4580a9f06768e0f93f22
Ben Church <ben.douglas.church@gmail.com>
2020-02-04 15:23:02 -0700

    Initial Commit

---
+++ lib/auth0_jwks.ex
@@ -0,0 +1,18 @@
+defmodule Auth0Jwks do
+  @moduledoc """
+  Documentation for `Auth0Jwks`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Auth0Jwks.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/auth0_jwks_test.exs
@@ -0,0 +1,8 @@
+defmodule Auth0JwksTest do
+  use ExUnit.Case
+  doctest Auth0Jwks
+
+  test "greets the world" do
+    assert Auth0Jwks.hello() == :world
+  end
+end
