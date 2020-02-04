eecbd1a03bf7766bbd96ef9fb39001ad775c4a5b
Ben Church <ben.douglas.church@gmail.com>
2020-02-04 15:31:56 -0700

    Add written code

--- mix.exs
+++ mix.exs
@@ -21,8 +21,9 @@ defmodule Auth0Jwks.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:joken, "~> 2.0"},
+      {:joken_jwks, "~> 1.1.0"},
+      {:plug_cowboy, "~> 2.0"},
     ]
   end
 end
